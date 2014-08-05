9401a3ca0a49f701113d74dac53deff30f22c49e
Max Resnick <maxongithub@fastmail.fm>
2014-08-04 22:16:19 -0700

    adding manytomanyfield stuff.

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from mezzanine.forms.models import Form
 from mezzanine.pages.models import Page, RichText
+from portfolio.models import PortfolioItem
 
 
 class ContactLandingPage(Page, RichText):
     """
-    LandngPage Class Model
+    Landng Page Model
     """
     tracking_code = models.TextField(verbose_name="Tracking Code Snippet.")
     contact_form = models.ForeignKey(Form, verbose_name="Contact Form.")
+    portfolio_item = models.ManyToManyField(PortfolioItem, null=True, verbose_name="Portfolio Item")
+    # TODO Set clean to stop anything over 3.
